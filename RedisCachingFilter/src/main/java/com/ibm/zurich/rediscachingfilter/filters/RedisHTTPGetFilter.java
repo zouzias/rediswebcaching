5ba87b58e62dd92b4276530d24466d1bc764f356
zouzias <zouzias@gmail.com>
2014-08-08 16:17:33 +0200

    Added content

---
+++ RedisCachingFilter/src/main/java/com/ibm/zurich/rediscachingfilter/filters/RedisHTTPGetFilter.java
@@ -0,0 +1,298 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ibm.zurich.rediscachingfilter.filters;
+
+import com.ibm.zurich.rediscachingfilter.redis.RedisConnector;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Enumeration;
+import java.util.Map;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import org.eclipse.jetty.http.HttpMethod;
+
+/**
+ *
+ * @author azo
+ */
+public class RedisHTTPGetFilter implements Filter {
+
+    private static final boolean debug = true;
+
+    // The filter configuration object we are associated with.  If
+    // this value is null, this filter instance is not currently
+    // configured. 
+    private FilterConfig filterConfig = null;
+
+    private String hashURL = null;
+    private String cachedContent = null;
+
+    public RedisHTTPGetFilter() {
+    }
+
+    private void doBeforeProcessing(ServletRequest request, ServletResponse response)
+            throws IOException, ServletException {
+        if (debug)
+            log("RedisHTTPGetFilter:DoBeforeProcessing");
+
+        HttpServletRequest req = (HttpServletRequest) request;
+
+        // Write code here to process the request and/or response before
+        // the rest of the filter chain is invoked.
+        // For example, a logging filter might log items on the request object,
+        // such as the parameters.
+        /*
+         String value = RedisConnector.getKey(urlString);
+         if ( value == null){
+         URL url = new URL(urlString);
+                
+         InputStream is = url.openStream();
+         value = IOUtils.toString(is, "UTF-8");
+         RedisConnector.addKey(urlString, value, true);
+         }
+        
+         */
+        StringBuilder builder = new StringBuilder();
+
+        // If method is GET
+        if (req.getMethod().compareTo(HttpMethod.GET.asString()) == 0) {
+
+            Map<String, String[]> params = req.getParameterMap();
+            for (String param : params.keySet()) {
+                for (String value : params.get(param)) {
+                    if (builder.length() == 0) {
+                        builder.append(param).append("=").append(value);
+                    } else {
+                        builder.append("&").append(param).append("=").append(value);
+                    }
+                }
+            }
+
+            hashURL = req.getRequestURI() + builder.toString();
+            cachedContent = RedisConnector.getKey(hashURL);
+
+        } else {
+            hashURL = null;
+        }
+
+        /* 
+         if ( value == null){
+         URL url = new URL(urlString);
+                
+         InputStream is = url.openStream();
+         value = IOUtils.toString(is, "UTF-8");
+         }
+         */
+        for (Enumeration en = request.getParameterNames(); en.hasMoreElements();) {
+            String name = (String) en.nextElement();
+            String values[] = request.getParameterValues(name);
+            int n = values.length;
+            StringBuilder buf = new StringBuilder();
+            buf.append(name);
+            buf.append("=");
+            for (int i = 0; i < n; i++) {
+                buf.append(values[i]);
+                if (i < n - 1)
+                    buf.append(",");
+            }
+            log(buf.toString());
+        }
+
+    }
+
+    private void doAfterProcessing(ServletRequest request, ServletResponse response)
+            throws IOException, ServletException {
+        if (debug)
+            log("RedisHTTPGetFilter:DoAfterProcessing");
+
+        boolean isJSON = response.getContentType().toLowerCase().contains("text");
+
+        if (hashURL != null && isJSON) {
+
+            RedisConnector.addKey(hashURL, response.getWriter().toString(), true);
+
+        }
+
+	// Write code here to process the request and/or response after
+        // the rest of the filter chain is invoked.
+        // For example, a logging filter might log the attributes on the
+        // request object after the request has been processed. 
+	/*
+         for (Enumeration en = request.getAttributeNames(); en.hasMoreElements(); ) {
+         String name = (String)en.nextElement();
+         Object value = request.getAttribute(name);
+         log("attribute: " + name + "=" + value.toString());
+
+         }
+         */
+        // For example, a filter might append something to the response.
+	/*
+         PrintWriter respOut = new PrintWriter(response.getWriter());
+         respOut.println("<P><B>This has been appended by an intrusive filter.</B>");
+         */
+    }
+
+    /**
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain The filter chain we are processing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+
+        if (debug)
+            log("RedisHTTPGetFilter:doFilter()");
+
+        doBeforeProcessing(request, response);
+
+        if (cachedContent != null) {
+
+            response.getWriter().write(cachedContent);
+            return;
+
+        } else {
+
+            Throwable problem = null;
+            try {
+                chain.doFilter(request, response);
+            } catch (Throwable t) {
+                // If an exception is thrown somewhere down the filter chain,
+                // we still want to execute our after processing, and then
+                // rethrow the problem after that.
+                problem = t;
+                t.printStackTrace();
+            }
+
+            // If there was a problem, we want to rethrow it if it is
+            // a known type, otherwise log it.
+            if (problem != null) {
+                if (problem instanceof ServletException)
+                    throw (ServletException) problem;
+                if (problem instanceof IOException)
+                    throw (IOException) problem;
+                sendProcessingError(problem, response);
+            }
+        }
+
+        doAfterProcessing(request, response);
+
+    }
+
+    /**
+     * Return the filter configuration object for this filter.
+     *
+     * @return
+     */
+    public FilterConfig getFilterConfig() {
+        return (this.filterConfig);
+    }
+
+    /**
+     * Set the filter configuration object for this filter.
+     *
+     * @param filterConfig The filter configuration object
+     */
+    public void setFilterConfig(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+    }
+
+    /**
+     * Destroy method for this filter
+     */
+    @Override
+    public void destroy() {
+    }
+
+    /**
+     * Init method for this filter
+     *
+     * @param filterConfig
+     */
+    @Override
+    public void init(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+        if (filterConfig != null) {
+            if (debug) {
+                log("RedisHTTPGetFilter:Initializing filter");
+            }
+        }
+    }
+
+    /**
+     * Return a String representation of this object.
+     */
+    @Override
+    public String toString() {
+        if (filterConfig == null)
+            return ("RedisHTTPGetFilter()");
+        StringBuilder sb = new StringBuilder("RedisHTTPGetFilter(");
+        sb.append(filterConfig);
+        sb.append(")");
+        return (sb.toString());
+    }
+
+    private void sendProcessingError(Throwable t, ServletResponse response) {
+        String stackTrace = getStackTrace(t);
+
+        if (stackTrace != null && !stackTrace.equals("")) {
+            try {
+                response.setContentType("text/html");
+                PrintStream ps = new PrintStream(response.getOutputStream());
+                PrintWriter pw = new PrintWriter(ps);
+                pw.print("<html>\n<head>\n<title>Error</title>\n</head>\n<body>\n"); //NOI18N
+
+                // PENDING! Localize this for next official release
+                pw.print("<h1>The resource did not process correctly</h1>\n<pre>\n");
+                pw.print(stackTrace);
+                pw.print("</pre></body>\n</html>"); //NOI18N
+                pw.close();
+                ps.close();
+                response.getOutputStream().close();
+            } catch (Exception ex) {
+            }
+        } else {
+            try {
+                PrintStream ps = new PrintStream(response.getOutputStream());
+                t.printStackTrace(ps);
+                ps.close();
+                response.getOutputStream().close();
+            } catch (Exception ex) {
+            }
+        }
+    }
+
+    public static String getStackTrace(Throwable t) {
+        String stackTrace = null;
+        try {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            t.printStackTrace(pw);
+            pw.close();
+            sw.close();
+            stackTrace = sw.getBuffer().toString();
+        } catch (Exception ex) {
+        }
+        return stackTrace;
+    }
+
+    public void log(String msg) {
+        filterConfig.getServletContext().log(msg);
+    }
+
+}
---
+++ RedisCachingFilter/src/main/java/com/ibm/zurich/rediscachingfilter/servlets/HelloServlet.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ibm.zurich.rediscachingfilter.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author azo
+ */
+public class HelloServlet extends HttpServlet {
+
+    private String greeting="Hello World";
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+           response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("<h1>"+greeting+"</h1>");
+        response.getWriter().println("session=" + request.getSession(true).getId());
+        
+    }
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
